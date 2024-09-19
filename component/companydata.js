187588c2f54ee449c7b30de05695d8876770aa8c
Ashok Kumar Shrestha <59673063+ashokstha61@users.noreply.github.com>
2024-09-19 18:18:20 +0545

    first commit

---
+++ component/companydata.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+const CollegeData = ()=>{
+    return(
+      <View>
+        <Text style={{fontSize:25}}>Name: abc</Text>
+        <Text style={{fontSize:25}}>Address: humanti</Text>
+        <Text style={{fontSize:25}}>Email: abc@gmail.com</Text>
+      </View>
+    );
+  };
+export default CollegeData;
