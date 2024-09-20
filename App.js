2242df9db94ecf2eb6e0b51606dc52448f8c4fb6
Ashok Kumar Shrestha <59673063+ashokstha61@users.noreply.github.com>
2024-09-20 08:25:18 +0545

    use state

--- App.js
+++ App.js
@@ -5,65 +5,29 @@
  * @format
  */
 
-import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
-{/*import { Text ,View,  StyleSheet, Button } from 'react-native';
-import CollegeData from './component/companydata';
-let age = 20;
-var email = 'abc@gmail.com';
-function fruit(){
-  return 'mango';
-}*/}
+import React, {Component, useState} from 'react';
+import {Button, Text, View} from 'react-native';
 
-function App(){
-  {/*
-    const buton = ()=>{
-    console.warn('Pressed on button.');
-  };
-  const button2 = ()=>{
-    console.warn('pressed here');
-  };
-  const button3 = (val)=>{
-    console.warn(val);
-  };
-  */}
-  
-  const [name,setName] = useState('anil');
-  function testName(){
+function App() {
+  const [name, setName] = useState('anil');
+  function testName() {
     setName('sagar');
   }
   return (
     <View>
-      {/*<Text style={styles.text}>Hello Text hello</Text>
-      <Text style={styles.text}>Bye bye</Text>
-      <Text style={styles.text}>{fruit()}</Text>
-      <Text style={styles.text}>{email}</Text>
-      <Text style={styles.age}> {age}</Text>
-      <Text style={styles.text}>{age === 25 ? 'above 20' : 'Unfit age'}</Text>
-      <Button title="Press Here" onPress={buton} color={'green'}/>
-      <Text style={styles.button}> Hello Component</Text>
-      <Button title="Press me" onPress={()=>button2()}/>
-      <UserData/>
-      <CollegeData/>
-      <Button title="Press me" onPress={()=>button3('Hello ram')} color={'green'}/>*/}
-      <Text style={{fontSize:20}}>{name}</Text>
-      <Button title='Update name' onPress={testName}/>
+      <Text style={{fontSize: 20}}>{name}</Text>
+      <Button title="Update name" onPress={testName} />
+      <User name={'ram'} />
     </View>
   );
 }
-{/*
-  const UserData = ()=>{
-return(
-  <View>
-    <Text style={{fontSize:25}}>Name: Hari</Text>
-    <Text style={{fontSize:25}}>age: 26</Text>
-    <Text style={{fontSize:25}}>Email: Hari@gmail.com</Text>
-  </View>
-);
+
+const User = props => {
+  return (
+    <View style={{backgroundColor: 'green', padding: 5}}>
+      <Text style={{fontSize: 25}}>{props.name}</Text>
+    </View>
+  );
 };
-*/}
-
-
-
 
 export default App;
